743bdae30408578a84a9a70f48c3a6ea3cbd504d
Hugo ALLIAUME <kocal@live.fr>
2016-05-31 11:49:52 +0200

    Test/Engine: done

--- tests/engine.js
+++ tests/engine.js
@@ -11,31 +11,141 @@ describe("`ProgressBarModuleEngine`", function () {
 
 describe("`ProgressBarModuleEngine::constructor($container, options)`", function () {
 
-    it("should be instantiable", function () {
-        new ProgressBarModuleEngine()
+    it("should throw a TypeError because there is no `$containe`r", function () {
+        expect(function () {
+            new ProgressBarModuleEngine()
+        }).toThrow(new TypeError('You must pass an HTML element as container during `ProgressBarModuleEngine`'
+                                 + ' instantiation.'));
+    });
+
+    it("should throw a TypeError because `$container` is not an HTMLElement", function () {
+        expect(function () {
+            new ProgressBarModuleEngine('not an HTML element')
+        }).toThrow(new TypeError('You must pass an HTML element as container during `ProgressBarModuleEngine`'
+                                 + ' instantiation.'));
+    });
+
+    it("should not throw a TypeError because `$container` is an HTMLElement", function () {
+        expect(function () {
+            var $container = document.createElement('div');
+            new ProgressBarModuleEngine($container);
+        }).not.toThrow(new TypeError('You must pass an HTML element as container during `ProgressBarModuleEngine`'
+                                     + ' instantiation.'));
+    });
+
+    it("should throw a TypeError because `options` is not an Object instance", function () {
+        expect(function () {
+            var $container = document.createElement('div');
+            new ProgressBarModuleEngine($container, 'not an object');
+        }).toThrow(new TypeError('You must pass an Object as options during `ProgressBarModuleEngine` instantiation.'));
+    });
+
+    it("should not throw a TypeError because `options` is an Object instance", function () {
+        expect(function () {
+            var $container = document.createElement('div');
+            new ProgressBarModuleEngine($container, { my: 'object' });
+        }).not.toThrow(new TypeError('You must pass an Object as options during `ProgressBarModuleEngine`'
+                                     + ' instantiation.'));
     });
 
 });
 
-describe("`ProgressBarModuleEngine` methods", function () {
+describe("`ProgressBarModuleEngine` methods should be overridden.", function () {
+    function MyEngine() {
+        return MyEngine.__super__.constructor.apply(this, arguments);
+    }
 
-    beforeEach(function () {
-        myEngine = (function (superClass) {
-                extend(myEngine, superClass);
+    extend(MyEngine, ProgressBarModuleEngine);
+    var myEngine = new MyEngine(document.createElement('div'), { my: 'object' });
 
-            function myEngine(container, options) {
-                myEngine.__super__.constructor.call(this, container, options);
-                this._settings = {};
-            }
-
-                return myEngine;
-            })(ProgressBarModuleEngine);
+    it("`ProgressBarModuleEngine::render()` should be overridden.", function () {
+        expect(function () {
+            myEngine.render();
+        }).toThrow(new Error('`render` method should be overridden.'));
     });
 
-    it("should be overridden", function () {
+
+    it("`ProgressBarModuleEngine::onInit(data)` should be overridden.", function () {
         expect(function () {
-            new ProgressBarModuleEngine()
-        }).toThrowError('`ProgressBarModuleEngine` should be implemented as an interface.')
+            myEngine.onInit();
+        }).toThrow(new Error('`onInit` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::onUpdate(data)` should be overridden.", function () {
+        expect(function () {
+            myEngine.onUpdate();
+        }).toThrow(new Error('`onUpdate` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::updateLabel(label)` should be overridden.", function () {
+        expect(function () {
+            myEngine.updateLabel();
+        }).toThrow(new Error('`updateLabel` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::updateProgression(progression)` should be overridden.", function () {
+        expect(function () {
+            myEngine.updateProgression();
+        }).toThrow(new Error('`updateProgression` method should be overridden.'));
+    });
+
+});
+
+describe("`ProgressBarModuleEngine` methods should be overridden.", function () {
+    function MyEngine() {
+        return MyEngine.__super__.constructor.apply(this, arguments);
+    }
+
+    extend(MyEngine, ProgressBarModuleEngine);
+
+    MyEngine.prototype.render = function () {
+    };
+    MyEngine.prototype.onInit = function () {
+    };
+    MyEngine.prototype.onUpdate = function () {
+    };
+    MyEngine.prototype.updateLabel = function () {
+    };
+    MyEngine.prototype.updateProgression = function () {
+    };
+
+    var myEngine = new MyEngine(document.createElement('div'), { my: 'object' });
+
+    it("`ProgressBarModuleEngine::render()` should be overridden.", function () {
+        expect(function () {
+            myEngine.render();
+        }).not.toThrow(new Error('`render` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::onInit(data)` should be overridden.", function () {
+        expect(function () {
+            myEngine.onInit();
+        }).not.toThrow(new Error('`onInit` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::onUpdate(data)` should be overridden.", function () {
+        expect(function () {
+            myEngine.onUpdate();
+        }).not.toThrow(new Error('`onUpdate` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::updateLabel(label)` should be overridden.", function () {
+        expect(function () {
+            myEngine.updateLabel();
+        }).not.toThrow(new Error('`updateLabel` method should be overridden.'));
+    });
+
+
+    it("`ProgressBarModuleEngine::updateProgression(progression)` should be overridden.", function () {
+        expect(function () {
+            myEngine.updateProgression();
+        }).not.toThrow(new Error('`updateProgression` method should be overridden.'));
     });
 
 });
